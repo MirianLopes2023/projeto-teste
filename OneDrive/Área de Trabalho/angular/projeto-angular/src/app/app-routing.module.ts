8aac5123765e90b6f3298e1e478e2d7a8268aae4
Mirian Lopes <mirianevidalopes@gmail.com>
2024-09-13 15:42:56 -0400

    Realizei algumas correções

--- OneDrive/Área de Trabalho/angular/projeto-angular/src/app/app-routing.module.ts
+++ OneDrive/Área de Trabalho/angular/projeto-angular/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
   { path:'inicio', component: InicioComponent},
